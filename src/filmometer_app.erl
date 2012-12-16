29cea89222711fd4916b4004593ad3503bef242e
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2012-12-16 20:56:52 +1100

    Initial commit

---
+++ src/filmometer_app.erl
@@ -0,0 +1,21 @@
+%% @author author <author@example.com>
+%% @copyright YYYY author.
+
+%% @doc Callbacks for the filmometer application.
+
+-module(filmometer_app).
+-author('author <author@example.com>').
+
+-behaviour(application).
+-export([start/2,stop/1]).
+
+
+%% @spec start(_Type, _StartArgs) -> ServerRet
+%% @doc application start callback for filmometer.
+start(_Type, _StartArgs) ->
+    filmometer_sup:start_link().
+
+%% @spec stop(_State) -> ServerRet
+%% @doc application stop callback for filmometer.
+stop(_State) ->
+    ok.
