dee4c2b66c9e737ffc0e42495bd2f1ca8a0a93b6
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2012-12-20 20:42:43 +1100

    Modified the default resource to query titles from OMDB

--- src/filmometer_resource.erl
+++ src/filmometer_resource.erl
@@ -3,11 +3,19 @@
 %% @doc Example webmachine_resource.
 
 -module(filmometer_resource).
--export([init/1, to_html/2]).
+-export([init/1, content_types_provided/2, to_json/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
-init([]) -> {ok, undefined}.
+init([]) -> 
+	{ok, undefined}.
 
-to_html(ReqData, State) ->
-    {"<html><body>Hello, new world</body></html>", ReqData, State}.
+content_types_provided(ReqData, Context) ->
+	{[{"application/json", to_json}], ReqData, Context}.
+
+to_json(ReqData, Context) ->
+    Title = wrq:get_qs_value("title", ReqData),
+    EncodedTitle = mochiweb_util:urlencode([{"t", Title}]),
+    RequestUri = string:concat("http://www.omdbapi.com/?", EncodedTitle),
+    {ok, {{Version, 200, ReasonPhrase}, Headers, Body}} = httpc:request(get, {RequestUri, []}, [], []),
+    {Body, ReqData, Context}.
