885603a4b69bf0b97d38f66b3822ad8fafbfad8b
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2012-12-20 22:12:23 +1100

    Refactored request to OMDB by making it async

--- src/filmometer_resource.erl
+++ src/filmometer_resource.erl
@@ -17,5 +17,14 @@ to_json(ReqData, Context) ->
     Title = wrq:get_qs_value("title", ReqData),
     EncodedTitle = mochiweb_util:urlencode([{"t", Title}]),
     RequestUri = string:concat("http://www.omdbapi.com/?", EncodedTitle),
-    {ok, {{Version, 200, ReasonPhrase}, Headers, Body}} = httpc:request(get, {RequestUri, []}, [], []),
+    {ok, RequestId} = httpc:request(get, {RequestUri, []}, [], [{sync, false}]),
+    Result = wait_for_response(RequestId),
+    {{_Version, 200, _ReasonPhrase}, _Headers, Body} = Result,
     {Body, ReqData, Context}.
+
+ wait_for_response(RequestId) ->
+ 	receive 
+ 		{http, {RequestId, Result}} -> Result 
+ 	after 
+ 		5000 -> timeout 
+ 	end.
