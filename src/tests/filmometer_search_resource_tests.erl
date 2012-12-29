eaa20bbdb813ea4b9a0cf771498511753fc9c98a
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2012-12-29 20:15:49 +1100

    Added another test for combine_results

--- src/tests/filmometer_search_resource_tests.erl
+++ src/tests/filmometer_search_resource_tests.erl
@@ -15,7 +15,23 @@ combine_results_should_return_an_empty_list_if_there_is_no_result_from_omdb_test
 	Actual = filmometer_search_resource:combine_results([#movie{source="Flixster"}]),
 	?assert(Actual =:= Expected).
 
-
+combined_results_should_return_a_list_of_aggregated_results_if_there_is_a_result_from_omdb_test() ->
+	PropLists = [{struct, [{source,"OMDB"}, {title,"Sicko"}, {year,2007}, 
+						   {actors,"John Doe, Jane Doe"},
+						   {poster,"foo.bar/omdb.jpg"}, {rating,8.8}]}, 
+				 {struct, [{source,"TMDB"}, {title,"Sicko"}, {year,2007}, 
+				 		   {actors,"John Doe, Jane Doe"},
+				 		   {poster,"foo.bar/tmdb.jpg"}, {rating,9.0}]}],
+	Expected = {struct, [{"averageRating", 8.9}, 
+				         {"verdict", "Hellz yeah!"},
+				         {"poster", "foo.bar/tmdb.jpg"},
+				         {"ratings", {array, PropLists}}]},
+	Actual = filmometer_search_resource:combine_results(
+		[#movie{source="OMDB", title="Sicko", actors="John Doe, Jane Doe", 
+				year=2007, rating=8.8, poster="foo.bar/omdb.jpg"},
+		 #movie{source="TMDB", title="Sicko",  actors="John Doe, Jane Doe",
+		 		year=2007, rating=9.0, poster="foo.bar/tmdb.jpg"}]),
+	?assert(Actual =:= Expected).
 
 get_verdict_for_should_return_the_correct_verdict_for_any_number_between_0_and_2_test() ->
 	Expected = "There should be a crime against watching this",
